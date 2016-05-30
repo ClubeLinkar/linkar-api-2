0102d2c3d2957e5fe52f9bbf6563d9597c411b6d
Lennon Jesus <lennon.jesus@gmail.com>
2016-05-30 07:35:54 -0300

    adicionando user a company

--- models/company.js
+++ models/company.js
@@ -5,7 +5,13 @@ var companySchema = new Schema({
   name: String,
   description: String,
   cnpj: {type: String, unique: true},
-  categories: [String]
+  categories: [String],
+
+  // isso vai virar User
+  email: {type: String, unique: true},
+  password: String,
+  cnpj: {type: String, unique: true},
+  role: {type: String, default: 'COMPANY'}
 });
 
 module.exports = mongoose.model('Company', companySchema)
