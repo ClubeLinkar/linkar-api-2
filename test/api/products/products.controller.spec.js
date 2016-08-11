02e0c571e1764bf895cf0c4653fe9006f4c3d869
Lennon Jesus <lennon.jesus@gmail.com>
2016-08-11 19:30:27 -0300

    tentando mockar a model

--- test/api/products/products.controller.spec.js
+++ test/api/products/products.controller.spec.js
@@ -1,24 +1,57 @@
+var mongoose = require('mongoose');
 var sinon = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
 
 var controller = require('../../../src/api/products/products.controller');
 
+var Product = require('../../../src/api/products/product');
+
+require('sinon-mongoose');
+
 describe("Products Controller", function() {
+
+  beforeEach(function() {
+    sinon.stub(Product, 'find');
+  });
+
+  afterEach(function() {
+    Product.find.restore();
+  });
 
   describe("List Products", function() {
 
-      it("Should not list products to unauthorized user ", function() {
+      // it("Should not list products to unauthorized user ", function() {
+      //
+      //   var req, res, spy;
+      //
+      //   req = res = {};
+      //   spy = res.json = sinon.spy();
+      //
+      //   controller.list(req, res);
+      //
+      //   expect(spy.calledWith({error: "Autenticação requerida."})).to.equal(true);
+      // });
 
-        var req, res, spy;
 
-        req = res = {};
-        spy = res.json = sinon.spy();
+      it('should list all products to an administrator user', function() {
+        var a = { name: 'AAAAA' };
+        var b = { name: 'BBBBB' };
+        var expectedProducts = [a, b];
+
+        Product.find.yields(null, expectedProducts);
+
+        var req = { params: { }, user: {role: 'ADMIN'} };
+        var res = {
+          json: sinon.stub()
+        };
 
         controller.list(req, res);
 
-        expect(spy.calledWith({error: "Autenticação requerida."})).to.equal(true);
+        sinon.assert.calledWith(res.json, expectedProducts);
       });
 
+
   });
+
 });
---
+++ test/api/products/products.fixtures.js
@@ -0,0 +1,8 @@
+module.exports.aValidProduct = aValidProduct;
+
+function aValidProduct () {
+  return {
+    name: 'Riser de Guidão Anker',
+    price: 99
+  };
+};
